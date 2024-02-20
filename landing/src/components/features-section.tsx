299a31c0604334d1866816fa2cd58088c7f3e504
markr <mark.rufino.io@gmail.com>
2024-02-21 04:45:40 +0800

    add hero image

--- landing/src/components/features-section.tsx
+++ landing/src/components/features-section.tsx
@@ -4,8 +4,10 @@ interface FeaturesSectionProps {}
 
 export default function FeaturesSection({}: FeaturesSectionProps) {
   return (
-    <div className="border-foreground m-8 flex flex-col border-2 p-8">
-      <h1 className="w-full bg-red-500 text-5xl">Features</h1>
+    <div className="border-foreground m-8 flex flex-col border p-8">
+      <h1 className="text-foreground/70 w-full text-6xl font-medium">
+        Features
+      </h1>
     </div>
   );
 }
--- landing/src/components/hero-section.tsx
+++ landing/src/components/hero-section.tsx
@@ -1,19 +1,21 @@
 import { Button } from "./ui/button";
+import Image from "next/image";
+import HeroImage from "@/../public/hero-image.jpg";
 
 interface HeroSectionProps {}
 
 export default function HeroSection({}: HeroSectionProps) {
   return (
-    <div className="flex flex-col items-center text-center">
-      <h1 className="pt-2 text-4xl font-bold md:pt-32 md:text-5xl">
-        Instantly create a directory website
+    <section className="md: flex flex-col items-center gap-2 p-4 text-center">
+      <h1 className="text-3xl font-extrabold md:pt-16 md:text-6xl">
+        Instantly create your directory website
       </h1>
       <p className="text-foreground/50 text-md max-w-2xl p-2 md:text-xl">
-        Directory Maker Pro quickly gets the hard parts done, so you can launch
-        your idea right away (before others do!) — see the short{" "}
-        <span className="underline">demo</span>
+        Directory Maker Pro quickly gets the difficult parts done, so you can
+        launch your directory idea right away (before others do!) — see the
+        short <span className="underline">demo</span>
       </p>
-      <div className="mt-4 flex flex-col justify-center gap-3">
+      <div className="flex flex-col justify-center gap-3">
         <Button variant="outline">Get started for free 🌱</Button>
         <Button>
           <p>
@@ -22,6 +24,12 @@ export default function HeroSection({}: HeroSectionProps) {
           </p>
         </Button>
       </div>
-    </div>
+      <Image
+        className="m-4 aspect-video rounded-lg"
+        src={HeroImage}
+        alt="hero_image"
+        width={"960"}
+      />
+    </section>
   );
 }
