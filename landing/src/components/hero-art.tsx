6e1487e4baf0e714a1a6ee1e7d5883f2acf77f49
markr <mark.rufino.io@gmail.com>
2024-02-24 21:30:39 +0800

    wip

--- landing/src/components/hero-art.tsx
+++ landing/src/components/hero-art.tsx
@@ -5,7 +5,5 @@ import Image from "next/image";
 interface HeroArtProps {}
 
 export default function HeroArt({}: HeroArtProps) {
-  return (
-    <div className="border-border rounded-md border-2 md:w-[50%]">hello</div>
-  );
+  return <div className="border-border rounded-md border-2 md:w-[50%]"></div>;
 }
--- landing/src/components/hero-section.tsx
+++ landing/src/components/hero-section.tsx
@@ -27,3 +27,33 @@ export default function HeroSection({}: HeroSectionProps) {
     </section>
   );
 }
+
+// import { Button } from "./ui/button";
+// import HeroImage from "@/../public/hero-image.jpg";
+// import Image from "next/image";
+// import HeroArt from "./hero-art";
+
+// interface HeroSectionProps {}
+
+// export default function HeroSection({}: HeroSectionProps) {
+//   return (
+//     <section className="flex min-h-screen flex-col gap-8 p-4 md:mx-20 md:flex-row">
+//       <div className="flex flex-col gap-2 md:w-[50%] md:justify-center">
+//         <h1 className="text-6xl font-extrabold">
+//           Make a Directory Website in Minutes
+//         </h1>
+//         <p className="text-xl font-thin">
+//           Directory Maker Pro uses AI to turn your data into a directory website
+//           in just a few clicks. —{" "}
+//           <span className="underline underline-offset-4">
+//             watch 10 second demo
+//           </span>
+//         </p>
+//         <Button className="mt-6 p-8 text-2xl">Get Started for Free</Button>
+//       </div>
+//       <div className="border-border flex flex-col gap-2 border-2 md:w-[50%] md:justify-center">
+//         <div className="h-[70%] min-h-96 bg-red-500">hello</div>
+//       </div>
+//     </section>
+//   );
+// }
